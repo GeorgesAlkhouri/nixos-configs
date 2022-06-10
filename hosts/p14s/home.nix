6fc4b46d234102a679d1971b304216a10bf566ce
Georges <georges.alkhouri@gmail.com>
2022-06-10 13:13:53 +0200

    feat: add gestures for libinput

--- hosts/p14s/home.nix
+++ hosts/p14s/home.nix
@@ -40,10 +40,10 @@
   home.file.".config/autostart/libinput-gestures.desktop".source = ''${pkgs.libinput-gestures}/share/applications/libinput-gestures.desktop'';
   # see https://github.com/bulletmark/libinput-gestures/blob/master/libinput-gestures.conf
   home.file.".config/libinput-gestures.conf".text = ''
-    # gesture swipe left 4	_internal ws_left
-    # gesture swipe right 4	_internal ws_right
-    # gesture swipe left 3	xdotool key alt+Right
-    # gesture swipe right 3 xdotool key alt+Left
+    gesture swipe right 4	_internal ws_left
+    gesture swipe left 4	_internal ws_right
+    gesture swipe left 3	xdotool key alt+Left
+    gesture swipe right 3 xdotool key alt+Right
     # gesture pinch in	xdotool key ctrl+F9
     # gesture pinch out	xdotool key ctrl+F9
   '';

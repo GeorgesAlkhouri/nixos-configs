c3a8b1abb194d294cc00b4a29088220969df10d3
Georges <georges.alkhouri@gmail.com>
2022-06-28 21:23:43 +0200

    feat: enable shell.nix auto activation

--- shell.nix
+++ shell.nix
@@ -1,5 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs; [ buildPackages.cargo buildPackages.rustc buildPackages.pre-commit ];
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs; [
+    buildPackages.cargo
+    buildPackages.rustc
+    buildPackages.pre-commit
+  ];
 }
